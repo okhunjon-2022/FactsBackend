6c64a9b1937e0e6e8ce2429890ae4adb5a46f7be
okhunjon-2022 <okhunjonmakhmudov.1998@gmail.com>
2024-11-22 22:56:53 +0500

    lorem

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://factreality.netlify.app",
     credentials: true, //enable set cookie
   })
 );
